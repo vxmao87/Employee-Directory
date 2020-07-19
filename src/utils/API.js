8739af92267a7da02505d87f8a99d3c9dd790cc8
Vinh Mao <vinhxmao87@gmail.com>
2020-07-18 17:50:45 -0700

    API utility file created

---
+++ src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BASEURL = "https://randomuser.me/api/?results=200&nat=u";
+
+export default {
+    getEmployees: function() {
+        return axios.get(BASEURL);
+    }
+}
