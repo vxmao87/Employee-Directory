aa59e1206c74ad44d49cdf4f5298f844cc9b129c
Vinh Mao <vinhxmao87@gmail.com>
2020-07-31 17:07:02 -0700

    preparing form and container JS and CSS code

--- src/pages/Directory.js
+++ src/pages/Directory.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import API from "../utils/API";
 import Container from "../components/Container";
 import Jumbotron from "../components/Jumbotron";
+import Form from "../components/Form";
 
 class Directory extends Component {
     state = {
