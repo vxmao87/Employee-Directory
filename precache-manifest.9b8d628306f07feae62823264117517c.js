6758f30326ccdcac378c361d247a3adeaa3f94bb
Vinh Mao <vinhxmao87@gmail.com>
2020-08-20 17:28:00 -0700

    Updates

---
+++ precache-manifest.9b8d628306f07feae62823264117517c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7000ae51c7cf7d2dcca82f044b52310f",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "e73b1445994e4a239d1f",
+    "url": "/Employee-Directory/static/css/main.e257099d.chunk.css"
+  },
+  {
+    "revision": "7f9bcbdf832d90c70880",
+    "url": "/Employee-Directory/static/js/2.c8a8316d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.c8a8316d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e73b1445994e4a239d1f",
+    "url": "/Employee-Directory/static/js/main.087e2615.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
